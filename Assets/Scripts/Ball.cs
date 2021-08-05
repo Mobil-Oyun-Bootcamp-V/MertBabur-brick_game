91a98a848bd4c444ecf60b08143942655c2c3914
mertbabur <mertbabur1998@gmail.com>
2021-08-06 00:23:46 +0300

    top fırlatma eklendi.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] private float moveSpeed = 1f;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody2D.velocity = _rigidbody2D.velocity.normalized * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BallLauncher : MonoBehaviour
+{
+
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+
+    [SerializeField] private GameObject ballPrefab;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.back * -10;
+        
+        if (Input.GetMouseButtonDown(0)) // ekrana tıklandı ise
+        {
+            StartDrag(worldPosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            ContinueDrag(worldPosition);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            EndDrag(worldPosition);
+        }
+    }
+
+    private void StartDrag(Vector3 worldPosition)
+    {
+        _startPosition = worldPosition;
+    }
+
+    private void ContinueDrag(Vector3 worldPosition)
+    {
+        _endPosition = worldPosition;
+    }
+    
+    private void EndDrag(Vector3 worldPosition)
+    {
+        Vector3 direction = _endPosition - _startPosition;
+        direction.Normalize();
+
+        var ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        ball.GetComponent<Rigidbody2D>().AddForce(-direction);
+    }
+    
+}
