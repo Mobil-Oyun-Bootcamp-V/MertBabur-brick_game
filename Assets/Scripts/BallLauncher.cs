10c0811122a23f1ab6a91d76cfa01e1dca5d87ca
mertbabur <mertbabur1998@gmail.com>
2021-08-06 00:42:55 +0300

    yön için çubuk eklendi

--- Assets/Scripts/BallLauncher.cs
+++ Assets/Scripts/BallLauncher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,11 +7,17 @@ using UnityEngine.EventSystems;
 public class BallLauncher : MonoBehaviour
 {
 
-    private Vector3 _startPosition;
-    private Vector3 _endPosition;
+    private Vector3 _startDragPosition;
+    private Vector3 _endDragPosition;
+    private LaunchPreview _launchPreview;
 
     [SerializeField] private GameObject ballPrefab;
-    
+
+    private void Awake()
+    {
+        _launchPreview = GetComponent<LaunchPreview>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -33,17 +40,21 @@ public class BallLauncher : MonoBehaviour
 
     private void StartDrag(Vector3 worldPosition)
     {
-        _startPosition = worldPosition;
+        _startDragPosition = worldPosition;
+        _launchPreview.SetStartPoint(transform.position);
+        
     }
 
     private void ContinueDrag(Vector3 worldPosition)
     {
-        _endPosition = worldPosition;
+        _endDragPosition = worldPosition;
+        Vector3 direction = _endDragPosition - _startDragPosition;
+        _launchPreview.SetEndPoint(transform.position - direction);
     }
     
     private void EndDrag(Vector3 worldPosition)
     {
-        Vector3 direction = _endPosition - _startPosition;
+        Vector3 direction = _endDragPosition - _startDragPosition;
         direction.Normalize();
 
         var ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
--- Assets/Scripts/LaunchPreview.cs
+++ Assets/Scripts/LaunchPreview.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class LaunchPreview : MonoBehaviour
 {
+    private LineRenderer _lineRenderer;
+    private Vector3 _dragStartPoint;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _lineRenderer = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetStartPoint(Vector3 worldPoint)
     {
-        
+        _dragStartPoint = worldPoint;
+        _lineRenderer.SetPosition(0,_dragStartPoint);
     }
+
+    public void SetEndPoint(Vector3 worldPoint)
+    {
+        Vector3 pointOffset = worldPoint - _dragStartPoint;
+        Vector3 endPoint = transform.position + pointOffset;
+        
+        _lineRenderer.SetPosition(1, endPoint);
+    }
+    
 }
