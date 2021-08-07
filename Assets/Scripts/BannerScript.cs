e789bdc3690899761170dba87c182495bfb135ce
mertbabur <mertbabur1998@gmail.com>
2021-08-07 19:45:44 +0300

    banner eklendi

---
+++ Assets/Scripts/BannerScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class BannerScript : MonoBehaviour
+{
+    private BannerView _banner;
+
+    public String _bannerID = "ca-app-pub-3940256099942544/6300978111";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _banner = new BannerView(_bannerID, AdSize.Banner, AdPosition.Bottom);
+        AdRequest adReq = new AdRequest.Builder().Build();
+        _banner.LoadAd(adReq);
+        
+        _banner.Show();
+    }
+
+    
+}
